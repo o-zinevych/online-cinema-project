c676155958354c76fc8177fbbaf6d12a823eccc2
Olha <olgazinevych@gmail.com>
2025-08-18 11:45:05 +0300

    feat: implement generate_secure_token util function to be used as default value

--- src/security/utils.py
+++ src/security/utils.py
@@ -1,3 +1,5 @@
+import secrets
+
 from passlib.context import CryptContext
 
 pwd_context = CryptContext(schemes=["bcrypt"], bcrypt__rounds=14, deprecated="auto")
@@ -5,3 +7,7 @@ pwd_context = CryptContext(schemes=["bcrypt"], bcrypt__rounds=14, deprecated="au
 
 def hash_password(password: str) -> str:
     return pwd_context.hash(password)
+
+
+def generate_secure_token(length: int = 32) -> str:
+    return secrets.token_urlsafe(length)
