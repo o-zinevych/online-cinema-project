e7f3e409acdf65d5ac5c48072bc5c3c3cd0e6306
Olha <olgazinevych@gmail.com>
2025-08-26 11:50:43 +0300

    feat: add main file that includes accounts router

---
+++ src/main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+
+from routes import accounts
+
+app = FastAPI(
+    title="Cinema API",
+    description="An online cinema API that allows users to select, watch, and "
+    "purchase access to movies as well as manage their accounts.",
+)
+
+api_version_prefix = "/api/v1/cinema"
+
+app.include_router(
+    accounts.router, prefix=f"{api_version_prefix}/accounts", tags=["accounts"]
+)
