cc99bde13e8866e6cc9eb4add5ac8fb08a856e4b
Olha <olgazinevych@gmail.com>
2025-09-09 22:46:13 +0300

    feat: implement the send_password_reset_email method in EmailSenderInterface

--- src/notifications/interfaces.py
+++ src/notifications/interfaces.py
@@ -22,3 +22,16 @@ class EmailSenderInterface(ABC):
             email (str): The recipient's email address.
             login_link (str): The login link to include in the email.
         """
+
+    @abstractmethod
+    async def send_password_reset_email(
+        self, email: str, token: str, password_reset_link: str
+    ) -> None:
+        """
+        Send a password reset link asynchronously.
+
+        Args:
+            email (str): The recipient's email address.
+            token (str): The token to include in the password reset completion form.
+            password_reset_link (str): The password reset link to include in the email.
+        """
