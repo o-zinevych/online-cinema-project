90a180d9ff7cce7a61814f03cc2be6d0cc5eeb4b
Olha <olgazinevych@gmail.com>
2025-09-10 12:16:54 +0300

    feat: create a verify_password util function to check raw password against the hashed one

--- src/security/utils.py
+++ src/security/utils.py
@@ -9,5 +9,9 @@ def hash_password(password: str) -> str:
     return pwd_context.hash(password)
 
 
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    return pwd_context.verify(plain_password, hashed_password)
+
+
 def generate_secure_token(length: int = 32) -> str:
     return secrets.token_urlsafe(length)
