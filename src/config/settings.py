f737b06efd2b6fd7c930c32f19d77bb927a697e0
Olha <olgazinevych@gmail.com>
2025-09-14 18:18:54 +0300

    fix: change the default value of celery settings from localhost to 127.0.0.1 for testing

--- src/config/settings.py
+++ src/config/settings.py
@@ -27,7 +27,7 @@ class BaseAppSettings(BaseSettings):
     SECRET_KEY_REFRESH: str = os.getenv("SECRET_KEY_REFRESH", str(os.urandom(32)))
     JWT_SIGNING_ALGORITHM: str = os.getenv("JWT_SIGNING_ALGORITHM", "HS256")
 
-    CELERY_BROKER_URL: str = os.getenv("CELERY_BROKER_URL", "redis://localhost:6379/0")
+    CELERY_BROKER_URL: str = os.getenv("CELERY_BROKER_URL", "redis://127.0.0.1:6379/0")
     CELERY_RESULT_BACKEND: str = os.getenv(
-        "CELERY_RESULT_BACKEND", "redis://localhost:6379/0"
+        "CELERY_RESULT_BACKEND", "redis://127.0.0.1:6379/0"
     )
