c78652d7497ba932613886bc3c80b7ccd445ac4c
Olha <olgazinevych@gmail.com>
2025-09-17 09:45:07 +0300

    feat: add MovieListResponseSchema

--- src/schemas/movies.py
+++ src/schemas/movies.py
@@ -1,6 +1,11 @@
+from typing import List, Optional
+
 from pydantic import BaseModel, ConfigDict
 
-from schemas.examples.movies import movie_list_item_schema_example
+from schemas.examples.movies import (
+    movie_list_item_schema_example,
+    movie_list_response_schema_example,
+)
 
 
 class MovieListItemSchema(BaseModel):
@@ -13,3 +18,16 @@ class MovieListItemSchema(BaseModel):
     name: str
     year: int
     imdb_score: float
+
+
+class MovieListResponseSchema(BaseModel):
+    model_config = ConfigDict(
+        from_attributes=True,
+        json_schema_extra={"examples": movie_list_response_schema_example},
+    )
+
+    movies: List[MovieListItemSchema]
+    prev_page: Optional[str]
+    next_page: Optional[str]
+    total_pages: int
+    total_items: int
