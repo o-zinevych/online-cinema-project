531d21e48be90d6b292d649fbb53543dd3d91ae2
Olha <olgazinevych@gmail.com>
2025-09-17 09:44:29 +0300

    feat: create a movie list response schema example

--- src/schemas/examples/movies.py
+++ src/schemas/examples/movies.py
@@ -4,3 +4,11 @@ movie_list_item_schema_example = {
     "year": 1990,
     "imdb_score": 7.7,
 }
+
+movie_list_response_schema_example = {
+    "movies": [movie_list_item_schema_example],
+    "prev_page": "/cinema/movies/?page=1&per_page=10",
+    "next_page": "/cinema/movies/?page=2&per_page=10",
+    "total_pages": 49,
+    "total_items": 490,
+}
