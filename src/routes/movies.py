0106d9240ec601132c23f82919a8bdac5b48b95d
Olha <olgazinevych@gmail.com>
2025-09-17 11:23:09 +0300

    feat: implement movies list endpoint with pagination

--- src/routes/movies.py
+++ src/routes/movies.py
@@ -1,3 +1,83 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Query, Depends, HTTPException
+from sqlalchemy import select, func
+from sqlalchemy.ext.asyncio import AsyncSession
+from starlette import status
+
+from database import get_db
+from database.models.accounts import User
+from database.models.movies import Movie
+from schemas.movies import MovieListResponseSchema, MovieListItemSchema
+from security.account_utils import get_current_user
 
 router = APIRouter()
+
+
+@router.get(
+    "/movies/",
+    response_model=MovieListResponseSchema,
+    summary="Movie List",
+    description="Get the list of movies with pagination.",
+    status_code=status.HTTP_200_OK,
+    responses={
+        404: {
+            "description": "Not found - No movies found.",
+            "content": {
+                "application/json": {"example": {"detail": "No movies found."}}
+            },
+        },
+    },
+)
+async def get_movies(
+    page: int = Query(1, ge=1),
+    per_page: int = Query(10, ge=1, le=100),
+    db: AsyncSession = Depends(get_db),
+    current_user: User = Depends(get_current_user),
+) -> MovieListResponseSchema:
+    """
+    Movie list endpoint.
+
+    Retrieves the list of movies allowing the client to specify the page number and
+    the number of items per page. It also calculates the total number of pages and items.
+    Provides the links to previous and next pages when applicable.
+
+    Args:
+        page (int): Page number (must be >= 1).
+        per_page (int): Number of items per page (must be between 1 and 100).
+        db (AsyncSession): Asynchronous database session.
+        current_user (User): The authenticated user.
+
+    Returns:
+        MovieListResponseSchema: Movie list response.
+
+    Raises:
+        HTTPException:
+            - 404 if no movies are found.
+    """
+    no_movies_exception = HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND, detail="No movies found."
+    )
+    count_result = await db.execute(select(func.count(Movie.id)))
+    total_items = count_result.scalar() or 0
+    if not total_items:
+        raise no_movies_exception
+    offset = (page - 1) * per_page
+    movie_result = await db.execute(select(Movie).limit(per_page).offset(offset))
+    movies = movie_result.scalars().all()
+    if not movies:
+        raise no_movies_exception
+
+    movie_list = [MovieListItemSchema.model_validate(movie) for movie in movies]
+    total_pages = (total_items + per_page - 1) // per_page
+    return MovieListResponseSchema(
+        movies=movie_list,
+        prev_page=(
+            f"/cinema/movies/?page={page - 1}&per_page={per_page}" if page > 1 else None
+        ),
+        next_page=(
+            f"/cinema/movies/?page={page + 1}&per_page={per_page}"
+            if page < total_pages
+            else None
+        ),
+        total_pages=total_pages,
+        total_items=total_items,
+    )
