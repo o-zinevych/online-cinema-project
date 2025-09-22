eb0d64c82f0170137502368caf49d2245048c6a4
Olha <olgazinevych@gmail.com>
2025-09-22 11:33:57 +0300

    refactor: remove cinema from movies route

--- src/main.py
+++ src/main.py
@@ -13,6 +13,4 @@ api_version_prefix = "/api/v1/cinema"
 app.include_router(
     accounts.router, prefix=f"{api_version_prefix}/accounts", tags=["accounts"]
 )
-app.include_router(
-    movies.router, prefix=f"{api_version_prefix}/cinema", tags=["cinema"]
-)
+app.include_router(movies.router, prefix=f"{api_version_prefix}", tags=["cinema"])
