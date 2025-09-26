5de0bfeea83979053edb83465808c27d77f1d50b
Olha <olgazinevych@gmail.com>
2025-09-26 12:55:04 +0300

    feat: extend EmailSenderInterface to include reply notifications

--- src/notifications/interfaces.py
+++ src/notifications/interfaces.py
@@ -37,11 +37,25 @@ class EmailSenderInterface(ABC):
         """
 
     @abstractmethod
-    async def send_password_reset_complete_email(self, email: str, login_link: str) -> None:
+    async def send_password_reset_complete_email(
+        self, email: str, login_link: str
+    ) -> None:
         """
         Send a password reset complete email asynchronously.
 
         Args:
-        email (str): The recipient's email address.
-        login_link (str): The login link to include in the email.
+            email (str): The recipient's email address.
+            login_link (str): The login link to include in the email.
+        """
+
+    @abstractmethod
+    async def send_comment_received_reply_email(
+        self, email: str, replies_link: str
+    ) -> None:
+        """
+        Send an email notification about a new comment reply asynchronously.
+
+        Args:
+            email (str): The recipient's email address.
+            replies_link (str): The link to the reply to include in the email.'
         """
