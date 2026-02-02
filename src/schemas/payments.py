e12b0c006acba06d4a4517ad897fa2fe932e5f79
Olha <olgazinevych@gmail.com>
2026-02-02 20:21:53 +0200

    feat: create PaymentDetailSchema and PaymentListResponseSchema

--- src/schemas/payments.py
+++ src/schemas/payments.py
@@ -1,8 +1,31 @@
-from typing import Optional
+from datetime import datetime
+from decimal import Decimal
+from typing import Optional, List
 
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict
+
+from database.models.payments import PaymentStatusEnum
 
 
 class PaymentCheckoutResponseSchema(BaseModel):
     checkout_url: Optional[str] = None
     message: str = "Redirecting the user to checkout."
+
+
+class PaymentDetailSchema(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+
+    id: int
+    created_at: datetime
+    amount: Decimal
+    status: PaymentStatusEnum
+
+
+class PaymentListResponseSchema(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+
+    payments: List[PaymentDetailSchema]
+    prev_page: Optional[str]
+    next_page: Optional[str]
+    total_pages: int
+    total_items: int
